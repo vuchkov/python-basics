8f126b748ca6893c1d6952c4ce71ff3b4626059a
Ilcho <vuchkov24@gmail.com>
2024-06-01 10:39:24 +0300

    Update clock

--- 6/6/clock.py
+++ 6/6/clock.py
@@ -1,9 +1,13 @@
 hour = 0
 minutes = 0
 
-while hour < 24 and minutes < 60:
-    print(f"{hour}:{minutes}")
-    minutes += 1
-    if minutes == 60 and hour < 24:
-        minutes = 0
-        hour += 1
+# while hour < 24 and minutes < 60:
+#     print(f"{hour}:{minutes}")
+#     minutes += 1
+#     if minutes == 60 and hour < 24:
+#         minutes = 0
+#         hour += 1
+
+for hour in range(24):
+    for minutes in range(60):
+        print(f"{hour}:{minutes}")
